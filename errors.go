764b4207759d5d60582e7e0054d1b339b585a161
Maurice Courtois <mc@foxstone.ch>
2018-12-23 23:26:35 +0100

    - Adding errors

---
+++ errors.go
@@ -0,0 +1,15 @@
+package cigExchange
+
+import (
+	"net/http"
+)
+
+// NotFoundHandler returns an error when requested resourse / route is missing
+var NotFoundHandler = func(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		Respond(w, Message(false, "This resources was not found on our server"))
+		next.ServeHTTP(w, r)
+	})
+}
