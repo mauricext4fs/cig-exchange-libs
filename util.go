b432956d4a4645c139e0065e6afd46d69b2606e8
alexgblr <alexgblr>
2018-12-24 15:26:17 +0300

    offerings api

--- util.go
+++ util.go
@@ -1,15 +1,35 @@
 package cigExchange
 
 import (
-    "encoding/json"
-    "net/http"
+	"encoding/json"
+	"net/http"
 )
 
-func Message(status bool, message string) map[string]interface{} {
-    return map[string]interface{}{"status": status, "message": message}
+// apiError is a struct representing server error response
+type apiError struct {
+	Message string
+	Code    int
 }
 
-func Respond(w http.ResponseWriter, data map[string]interface{}) {
-    w.Header().Add("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(data)
+// Message creates api call response format
+func Message(status bool, message string) map[string]interface{} {
+	return map[string]interface{}{"status": status, "message": message}
+}
+
+// RespondWithError writes error message into http response
+func RespondWithError(w http.ResponseWriter, statusCode int, err error) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	resp := make(map[string]interface{})
+	resp["error"] = apiError{
+		Message: err.Error(),
+		Code:    statusCode,
+	}
+	json.NewEncoder(w).Encode(resp)
+}
+
+// Respond writes object into http response
+func Respond(w http.ResponseWriter, object interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(object)
 }
