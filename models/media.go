5207a780257b14848dc9eb854a60324263dc3193
alexgblr <alexgblr>
2019-05-11 18:00:41 +0300

    add offering_media.index to media api calls

--- models/media.go
+++ models/media.go
@@ -47,12 +47,18 @@ func (media *Media) GetMultilangFields() []string {
 	return []string{}
 }
 
+// MediaWithIndex contains Media struct with index from OfferingMedia
+type MediaWithIndex struct {
+	*Media
+	Index int32 `json:"index" gorm:"index"`
+}
+
 // OfferingMedia is a struct to represent an offering media link
 type OfferingMedia struct {
 	ID         string     `json:"id" gorm:"column:id;primary_key"`
 	OfferingID string     `json:"offering_id" gorm:"column:offering_id"`
 	MediaID    string     `json:"media_id" gorm:"column:media_id"`
-	Index      int        `json:"index" gorm:"column:index;default:100"`
+	Index      int32      `json:"index" gorm:"column:index;default:100"`
 	CreatedAt  time.Time  `json:"created_at" gorm:"column:created_at"`
 	UpdatedAt  time.Time  `json:"updated_at" gorm:"column:updated_at"`
 	DeletedAt  *time.Time `json:"-" gorm:"column:deleted_at"`
@@ -87,9 +93,28 @@ func GetMedia(mediaID string) (*Media, *cigExchange.APIError) {
 	return media, nil
 }
 
-// CreateMediaForOffering creates media and offering media link
-func CreateMediaForOffering(media *Media, offeringID string) *cigExchange.APIError {
+// GetOfferingMedia queries a single media from db
+func GetOfferingMedia(offeringID, mediaID string) (*OfferingMedia, *cigExchange.APIError) {
 
+	offeringMedia := &OfferingMedia{
+		OfferingID: offeringID,
+		MediaID:    mediaID,
+	}
+	db := cigExchange.GetDB().Where(offeringMedia).First(offeringMedia)
+	if db.Error != nil {
+		if db.RecordNotFound() {
+			return nil, cigExchange.NewInvalidFieldError("media_id, offering_id", "Offering media link with provided ids doesn't exist")
+		}
+		return nil, cigExchange.NewDatabaseError("Fetch offering media failed", db.Error)
+	}
+
+	return offeringMedia, nil
+}
+
+// CreateMediaForOffering creates media and offering media link
+func CreateMediaForOffering(mediaIndex *MediaWithIndex, offeringID string) *cigExchange.APIError {
+
+	media := mediaIndex.Media
 	// check that UUID is set
 	if len(offeringID) == 0 {
 		return cigExchange.NewInvalidFieldError("offering_id", "Offering id is invalid")
@@ -105,6 +130,7 @@ func CreateMediaForOffering(media *Media, offeringID string) *cigExchange.APIErr
 	mediaOffering := &OfferingMedia{
 		OfferingID: offeringID,
 		MediaID:    media.ID,
+		Index:      mediaIndex.Index,
 	}
 	db = cigExchange.GetDB().Create(mediaOffering)
 	if db.Error != nil {
@@ -130,15 +156,17 @@ func (media *Media) Update(update map[string]interface{}) *cigExchange.APIError 
 }
 
 // GetMediaForOffering queries all offering media objects for offering
-func GetMediaForOffering(offeringID string) (media []*Media, apiError *cigExchange.APIError) {
+func GetMediaForOffering(offeringID string) (media []*MediaWithIndex, apiError *cigExchange.APIError) {
 
-	media = make([]*Media, 0)
+	media = make([]*MediaWithIndex, 0)
 	// check that UUID is set
 	if len(offeringID) == 0 {
 		return media, cigExchange.NewInvalidFieldError("offering_id", "Offering id is invalid")
 	}
 
-	db := cigExchange.GetDB().Joins("JOIN offering_media on offering_media.media_id=media.id").Where("offering_media.offering_id=?", offeringID).Find(&media)
+	db := cigExchange.GetDB().Select("media.*, offering_media.index").
+		Joins("JOIN offering_media on offering_media.media_id=media.id").
+		Where("offering_media.offering_id=?", offeringID).Find(&media)
 	if db.Error != nil {
 		if !db.RecordNotFound() {
 			return
@@ -172,6 +200,17 @@ func DeleteOfferingMedia(mediaID string) *cigExchange.APIError {
 	}
 	if db.RowsAffected == 0 {
 		return cigExchange.NewInvalidFieldError("media_id", "Offering Media link with provided id doesn't exist")
+	}
+	return nil
+}
+
+// UpdateIndex updates OfferingMedia record in db
+func (offeringMedia *OfferingMedia) UpdateIndex(index int32) *cigExchange.APIError {
+
+	offeringMedia.Index = index
+	db := cigExchange.GetDB().Save(offeringMedia)
+	if db.Error != nil {
+		return cigExchange.NewDatabaseError("Failed to update offering media ", db.Error)
 	}
 	return nil
 }
